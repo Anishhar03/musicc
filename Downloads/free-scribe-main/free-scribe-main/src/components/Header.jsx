a9cc972756997520b6c1db241d81430730ac762a
Anishhar03 <anishharsh1971@gmail.com>
2024-12-23 05:05:34 +0530

    Your commit message

--- Downloads/free-scribe-main/free-scribe-main/src/components/Header.jsx
+++ Downloads/free-scribe-main/free-scribe-main/src/components/Header.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
     return (
         <header className='flex items-center justify-between gap-4 p-4'>
-            <a href="/"><h1 className='font-medium'>Free<span className='text-blue-400 bold'>Scribe</span></h1></a>
-            <div className='gap-4 flex items-center '>
-                <a href="https://bmc.link/smoljames" target='_blank' className='text-slate-600 cursor-pointer' rel="noreferrer">Donate</a>
+            <a href="/">
+                <h1 className='font-medium text-xl transition-transform duration-300 ease-in-out hover:text-blue-500 hover:scale-105'>
+                    ScribiDooo
+                </h1>
+            </a>
+            <div className='gap-4 flex items-center'>
                 <a href="/" className='flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400'>
                     <p>New</p>
                     <i className="fa-solid fa-plus"></i>
                 </a>
             </div>
         </header>
-    )
+    );
 }
--- Downloads/free-scribe-main/free-scribe-main/src/utils/translate.worker.js
+++ Downloads/free-scribe-main/free-scribe-main/src/utils/translate.worker.js
@@ -9,32 +9,28 @@ class MyTranslationPipeline {
         if (this.instance === null) {
             this.instance = pipeline(this.task, this.model, { progress_callback });
         }
-
         return this.instance;
     }
 }
 
 self.addEventListener('message', async (event) => {
     let translator = await MyTranslationPipeline.getInstance(x => {
-        self.postMessage(x)
-    })
-    console.log(event.data)
+        self.postMessage(x);  // Send progress updates
+    });
+
     let output = await translator(event.data.text, {
         tgt_lang: event.data.tgt_lang,
         src_lang: event.data.src_lang,
-
-        callback_function: x => {
+        callback_function: (x) => {
             self.postMessage({
                 status: 'update',
                 output: translator.tokenizer.decode(x[0].output_token_ids, { skip_special_tokens: true })
-            })
+            });
         }
-    })
-
-    console.log('HEHEHHERERE', output)
+    });
 
     self.postMessage({
         status: 'complete',
         output
-    })
-})
+    });
+});
