9dd24bae4fd7bfd31596847b14b81394678d4062
Anishhar03 <anishharsh1971@gmail.com>
2025-03-21 01:26:07 +0530

    Initial commit

---
+++ Downloads/Music-School-App-main/Music-School-App-main/src/app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import FeatureSection from "@/components/FeatureSection";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/95 antialiased text-white">
+      <HeroSection />
+      <FeatureSection />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+    </main>
+  );
+}
